8969ace2fd552392076d075403c57105bd287500
Anderen <kolyanmskr@gmail.com>
2025-09-25 18:54:31 +0300

    Start project

---
+++ Korotun_ios_test_task/Korotun_ios_test_task/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Korotun_ios_test_task
+//
+//  Created by Anderen on 25.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
